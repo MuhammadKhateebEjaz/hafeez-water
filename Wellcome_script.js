324aa3b4cebdc3ec9f0785d4e160495c9756e1d2
Muhammad Khateeb Ejaz <160773008+MuhammadKhateebEjaz@users.noreply.github.com>
2025-03-22 20:16:12 +0500

    Update and rename script.js to Wellcome_script.js

---
+++ Wellcome_script.js
@@ -0,0 +1,5 @@
+// This is a simple script to show an alert when the page loads
+window.onload = function() {
+    alert("Hello Welcome To Water Panda Premium Drinking Services Website  Hope You Like It!");
+};
+
